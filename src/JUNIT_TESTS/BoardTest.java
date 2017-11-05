45d5a3784a6101a9ba66b328768ad2ff6cdf213d
Eric Allen <ericallen@172.16.34.228>
2017-11-05 17:00:25 -0500

    This needs work. Not sure how to add an npc piece from the view.

--- src/JUNIT_TESTS/BoardTest.java
+++ src/JUNIT_TESTS/BoardTest.java
@@ -2,10 +2,14 @@ package JUNIT_TESTS;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import game.Board;
-
+import game.NPC;
+import game.Player;
+import game.Character;
 public class BoardTest {
 	
 	@Test
@@ -16,6 +20,24 @@ public class BoardTest {
 		}
 		else
 			assertEquals("Time should read 5",false,b1.getIsGameOver());
+
+	}
+	
+	@Test
+	public void testPlayer(){
+		Board b1 = new Board();
+		b1.getPlayer();
+		assertEquals("Player To String at the start","This player has lane:3 Their score is:0 Player Health:10",b1.getPlayer().toString());
+	}
+	@Test
+	public void testGetObstacles(){
+		//ArrayList<NPC> getObstacles() {
+		Board b1 = new Board();
+		ArrayList a1 = new ArrayList<NPC>(); // empty arraylist
+		b1.getObstacles();
+		System.out.println(b1.getObstacles());
+		
+		assertEquals("No obstacles have been created yet",a1,b1.getObstacles());
 
 	}
 
